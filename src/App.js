0b12a70875d947b22d3744592293b4a728c3a2c2
asiwajudev <olanrewajuranda@gmail.com>
2020-05-11 04:37:20 +0100

    Redux store created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
 
 import CardList from "./components/cardList/CardList";
 import SearchBox from "./components/searchBox/SearchBox";
 import Scroll from "./components/scroll/Scroll";
 import ErrorBoundary from "./components/errorBoundary/ErrorBoundary";
 
+import { searchRobots } from "./Reducer";
+
 import "./App.css";
+
+const store = createStore(searchRobots);
 
 class App extends Component {
   constructor() {
@@ -30,18 +36,20 @@ class App extends Component {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    return !robots.length ? (
-      <h1 className="tc">Loading</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
+    return (
+      <Provider store={store}>
+        !robots.length ? (<h1 className="tc">Loading</h1>) : (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
+          </Scroll>
+        </div>
+        );
+      </Provider>
     );
   }
 }
--- src/Reducer.js
+++ src/Reducer.js
@@ -1,4 +1,4 @@
-import { CHANGE_SEARCH_FIELD } from "./Constants";
+import { CHANGE_SEARCHFIELD } from "./Constants";
 
 const initialState = {
   searchField: "",
@@ -6,8 +6,9 @@ const initialState = {
 
 export const searchRobots = (state = initialState, action = {}) => {
   switch (action.type) {
-    case CHANGE_SEARCH_FIELD:
-      return { ...state, searchField: action.payload };
+    case CHANGE_SEARCHFIELD:
+      //   return { ...state, searchField: action.payload };
+      return Object.assign({}, state, { searchField: action.payload });
     default:
       return state;
   }
