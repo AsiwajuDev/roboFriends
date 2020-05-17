f828d0466354dfac9c022509805f2837a360867f
asiwajudev <olanrewajuranda@gmail.com>
2020-05-17 04:17:59 +0100

    Redux-Logger middleware applied

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -10,7 +11,8 @@ import { searchRobots } from "./Reducer";
 
 import "tachyons";
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
